5d3d78bc17fb4188da5fab4383e98038c40ba9a1
nascVi <vaiiho.nascimbene@gmail.com>
2020-07-24 11:38:22 +0200

    initial

---
+++ src/components/People.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const People = () => {
+    return (
+        <div>
+            <h3>People</h3>
+        </div>
+    )
+}
+
+export default People
---
+++ src/components/Planets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Planets = () => {
+    return (
+        <div>
+            <h3>Planets</h3>
+        </div>
+    )
+}
+
+export default Planets
