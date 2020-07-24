4e80ca0396f0d6a5bd5fc12b0ff96b91bb29d4ec
nascVi <vaiiho.nascimbene@gmail.com>
2020-07-24 14:46:44 +0200

    useQ people and planet for star wars info

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+
+import Person from './Person'
+
+const fetchPeople = async () => {
+    const res = await fetch('http://swapi.dev/api/people/')
+    return res.json()
+}
 
 const People = () => {
+    const { data, status } = useQuery('people', fetchPeople)
+    console.log(data)
+
     return (
         <div>
             <h3>People</h3>
+            <p>{status}</p>
+
+            {status === 'loading' && (
+                <div>Loading data...</div>
+            )}
+
+            {status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === 'success' && (
+                <div>{ data.results.map(person => <Person key={person.name} person={person} />)}</div>
+            )}
         </div>
     )
 }
